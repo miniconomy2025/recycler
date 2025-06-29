e8fd2d564e3643bd21228443b55fff9d898832ab
Temwa-BBD <temwa.nyirenda@vcontractor.co.za>
2025-06-29 14:59:43 +0200

    Removed commented out code

--- Recycler.API/Program.cs
+++ Recycler.API/Program.cs
@@ -5,22 +5,6 @@ builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
-
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Miniconomy Recycler", Version = "v1" });
-//     c.EnableAnnotations();
-//     c.CustomSchemaIds(type => type.ToString());
-// });
-
-
-// await new Startup(builder).ConfigureApplication();
-
-// builder.Services.RegisterAllTypes(typeof(IRepository<>), ServiceLifetime.Scoped);
-// builder.Services.AddScoped<IClaimValidationService, ClaimValidationService>();
-// builder.Services.AddMediatR(mediatRServiceConfiguration =>
-//     mediatRServiceConfiguration.RegisterServicesFromAssembly(typeof(GetClaimLineItemsForClaimQuery).Assembly));
-
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 var app = builder.Build();
