025ade5faf3816a1273fcd5e9b3becb33de286b9
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-06-30 05:58:37 +0200

    Init static values

---
+++ db/V2__seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO Role (id, name) VALUES
+  (1, 'Recycler'),
+  (2, 'Supplier'),
+  (4, 'Logistics'),
+  (5, 'Bank'),
+
+INSERT INTO RawMaterial (id, name, price) VALUES
+  (1, 'Copper', 45.00),
+  (2, 'Silicon', 55.00),
+  (3, 'Sand', 10.00),
+  (4, 'Plastic', 15.00),
+  (5, 'Aluminum', 25.00);
+
+INSERT INTO OrderStatus (id, name) VALUES
+  (1, 'Pending'),
+  (2, 'Approved'),
+  (3, 'Rejected'),
+  (4, 'Shipped'),
+  (5, 'Delivered');
+  (6, 'Refunded')
+
+INSERT INTO PhoneParts (id, name) VALUES
+  (1, 'Screen'),
+  (2, 'Case'),
+  (3, 'Battery'),
+
