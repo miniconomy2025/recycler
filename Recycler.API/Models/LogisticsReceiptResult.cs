7ca97603a227977e5e40263e9bdf49f6e67bfa8c
Nomcebo-Ndimande <sibongile@bbd.co.za>
2025-07-08 22:09:17 +0200

    Add LogisticsReceiptResult for structured response data

--- Recycler.API/Models/LogisticsReceiptResult.cs
+++ Recycler.API/Models/LogisticsReceiptResult.cs
@@ -7,6 +7,10 @@ namespace Recycler.API.Models
 {
     public class LogisticsReceiptResult
     {
-        
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public int PhonesReceived { get; set; }
+        public List<string> ReceivedPhoneModels { get; set; } = new();
+        public RecyclingResult? RecyclingResult { get; set; }
     }
 }
