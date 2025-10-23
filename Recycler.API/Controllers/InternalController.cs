729191bdf852c37d520b429f2307b7bc24773ac3
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-10-23 22:39:36 +0200

    A bunch of tools

--- Recycler.API/Controllers/InternalController.cs
+++ Recycler.API/Controllers/InternalController.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace Recycler.API;
 
@@ -30,4 +31,79 @@ public class InternalController(IHttpClientFactory factory) : ControllerBase
         var parsed = JsonDocument.Parse(json);
         return Ok(parsed);
     }
+
+    [HttpGet]
+    [Route("add-machine")]
+    public async Task<IActionResult> AddAMachine()
+    {
+        var response = await _http.PostAsJsonAsync("https://api.recycler.susnet.co.za/logistics", new
+        {
+            type = "DELIVERY",
+            items = new[] {new
+            {
+            name = "recycling_machine",
+                quantity = 1
+            } }
+        });
+        var json = await response.Content.ReadAsStringAsync();
+        var parsed = JsonDocument.Parse(json);
+        return Ok(parsed);
+    }
+
+    [HttpGet]
+    [Route("get-loans")]
+    public async Task<IActionResult> GetLoans()
+    {
+        var response = await _http.GetAsync("https://commercial-bank-api.subspace.site/api/loan");
+        var json = await response.Content.ReadAsStringAsync();
+        var parsed = JsonDocument.Parse(json);
+        return Ok(parsed);
+    }
+
+    [HttpGet]
+    [Route("check-frozen-account")]
+    public async Task<IActionResult> CheckFrozen()
+    {
+        var response = await _http.GetAsync("https://commercial-bank-api.subspace.site/api/account/me/frozen");
+        var json = await response.Content.ReadAsStringAsync();
+        var parsed = JsonDocument.Parse(json);
+        return Ok(parsed);
+    }
+
+    [HttpPost]
+    [Route("take-out-a-loan")]
+    public async Task<IActionResult> TakeOutALoan([FromBody] LoanRequest request)
+    {
+        var response = await _http.PostAsJsonAsync("https://commercial-bank-api.subspace.site/api/loan", new
+        {
+            amount = request.Amount
+        });
+        var json = await response.Content.ReadAsStringAsync();
+        var parsed = JsonDocument.Parse(json);
+        return Ok(parsed);
+    }
+
+    [HttpPost]
+    [Route("payback-a-loan")]
+    public async Task<IActionResult> PayBackALoan([FromBody] LoanPaymentRequest request)
+    {
+        var response = await _http.PostAsJsonAsync($"https://commercial-bank-api.subspace.site/api/loan/{request.loanNumber}/pay", new
+        {
+            amount = request.Amount
+        });
+        var json = await response.Content.ReadAsStringAsync();
+        var parsed = JsonDocument.Parse(json);
+        return Ok(parsed);
+    }
+}
+
+public class LoanRequest
+{
+    public decimal Amount { get; set; }
+}
+
+public class LoanPaymentRequest
+{
+    public long loanNumber { get; set; }
+    public decimal Amount { get; set; }
 }
