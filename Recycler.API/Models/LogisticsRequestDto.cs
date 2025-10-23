428c9ef9c82c6c7a890862f971990944ac53bcc9
JMHolmes-source <joshua.holmes@bbd.co.za>
2025-10-24 00:13:35 +0200

    whoops

--- Recycler.API/Models/LogisticsRequestDto.cs
+++ Recycler.API/Models/LogisticsRequestDto.cs
@@ -2,7 +2,7 @@ namespace Recycler.API.Models
 {
     public class LogisticsRequestDto
     {
-        public string? Id { get; set; } 
+        public object? Id { get; set; } 
         public string? Type { get; set; } 
         public List<LogisticsItemDto>? Items { get; set; }
     }
